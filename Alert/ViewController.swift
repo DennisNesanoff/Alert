7a3b7ab2adfc8a9bea8ea2676d01d1a3f5eebb13
Dennis Nesanoff <nndennisnn@gmail.com>
2019-12-30 20:50:44 +0300

    Initial Commit

---
+++ Alert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alert
+//
+//  Created by Dennis Nesanoff on 30.12.2019.
+//  Copyright © 2019 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
