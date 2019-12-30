dd265e6862a3fc7263e8ff9d5a7b2eb7334a2ff0
Dennis Nesanoff <nndennisnn@gmail.com>
2019-12-30 22:42:18 +0300

    test version

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let switchHide = UISwitch()
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.isHidden = true
+        
+        switchHide.frame = CGRect(x: 50, y: 50, width: 0, height: 0)
+        view.addSubview(switchHide)
+        
+        switchHide.setOn(false, animated: true)
+        switchHide.addTarget(self, action: #selector(switchChanged), for: .valueChanged)
     }
 
-
+    @IBAction func showButton(_ sender: UIButton) {
+        label.isHidden = false
+        label.text = "Hi"
+        showAlert(title: "Error", message: "Send message!", style: .alert)
+    }
+    
+    @objc func switchChanged() {
+        if switchHide.isOn {
+            label.isHidden = true
+            button.isUserInteractionEnabled = false
+        } else {
+            label.isHidden = false
+            button.isUserInteractionEnabled = true
+        }
+    }
+    
+    func  showAlert(title: String, message: String, style: UIAlertController.Style) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "ok", style: .default) { (action) in
+            let text = alert.textFields?.first?.text
+            self.label.text! += ", \(text!)!"
+        }
+        
+        alert.addTextField { (textField) in
+            textField.isSecureTextEntry = true
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
